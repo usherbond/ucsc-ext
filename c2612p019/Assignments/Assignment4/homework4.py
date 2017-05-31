e34bf421dd4e36cbf729e566568e626c801a4b44
roquealex <roquealex20@hotmail.com>
2017-05-30 19:27:39 -0500

    plots both but not together

--- c2612p019/Assignments/Assignment4/homework4.py
+++ c2612p019/Assignments/Assignment4/homework4.py
@@ -11,6 +11,8 @@ from scipy.stats import norm
 #excel_file = 'Assignment_2_Data_and_Template.xlsx'
 excel_file = 'short_a4.xlsx'
 
+#excel_file = 'Assignment_4_Data_and_Template.xlsx'
+
 data_frame = pd.read_excel(excel_file,sheetname='Training Data')
 print data_frame
 df_matrix = data_frame.as_matrix()
@@ -46,6 +48,114 @@ for i,typ in enumerate(T_type):
 T_type_kesler = T_type_kesler-1
 print T_type_kesler
 
+# Generating Xa and its pesudo inverse
+X0 = np.ones((X.shape[0],1),dtype=float)
+print X0
+print X0.shape
+print type(X0)
+Xa = np.append(X0,X,1)
+print Xa
+Xapinv = np.linalg.pinv(Xa)
+print Xapinv
+
+# Calculate the w's
+W_failure = np.dot(Xapinv, T_failure)
+print W_failure
+
+W_type = np.dot(Xapinv, T_type_kesler)
+'''
+W_type = np.array(
+[[9,10,11,12],
+ [5, 6, 7, 8],
+ [1, 2, 3, 4]]
+)
+'''
+print W_type
+
+#line_type = -1*W_type[:-1]/W_type[-1,0]
+#print  W_type[-1].astype(float)
+line_type = -1*W_type[:-1]/W_type[-1].astype(float)
+print line_type
+
+Xa_max = np.amax(Xa,axis=0)
+Xa_min = np.amin(Xa,axis=0)
+print Xa_max
+print Xa_min
+Xrange = np.array([Xa_min,Xa_max])
+#exit()
+print Xrange
+print Xrange.shape
+Xpoints = Xrange[:,:-1]
+print Xpoints
+Xd = np.dot(Xpoints,line_type)
+print Xrange[:,1]
+print Xd
+#
+
+# ploting:
+uniq = np.unique(T_type)
+print uniq
+Cset = [X[np.where(T_type==cl)[0]] for cl in uniq]
+print Cset
+
+col = ['r','b','g','y','c','m']
+alpha_cloud = 0.1
+#plt.subplot(2,2,1)
+for i,cl in enumerate(Cset) :
+	plt.scatter(cl[:,0],cl[:,1],color=col[i],marker="o",alpha=alpha_cloud)
+#plt.plot([60,61],[22,20],"k")
+#plt.plot(Xrange[:,1],Xd,"k")
+for i in uniq :
+	plt.plot(Xrange[:,1],Xd[:,i],col[i])
+plt.axis('equal')
+plt.xlabel('X1')
+plt.ylabel('X2')
+plt.grid()
+plt.show()
+
+print Xrange[:,1]
+print Xd
+
+'''
+# calculating parameters for plotting :
+line_failure = -1*W_failure[:-1]/W_failure[-1,0]
+#line = np.array([16,1])
+Xa_max = np.amax(Xa,axis=0)
+Xa_min = np.amin(Xa,axis=0)
+print Xa_max
+print Xa_min
+Xrange = np.array([Xa_min,Xa_max])
+print line_failure
+#exit()
+print Xrange
+print Xrange.shape
+Xpoints = Xrange[:,:-1]
+print Xpoints
+Xd = np.dot(Xpoints,line_failure)
+print Xrange[:,1]
+print Xd
+#
+
+# ploting:
+uniq = np.unique(T_failure)
+print uniq
+Cset = [X[np.where(T_failure==cl)[0]] for cl in uniq]
+print Cset
+
+
+col = ['r','b','g','y','c','m']
+alpha_cloud = 0.1
+#plt.subplot(2,2,1)
+for i,cl in enumerate(Cset) :
+	plt.scatter(cl[:,0],cl[:,1],color=col[i],marker="o",alpha=alpha_cloud)
+#plt.plot([60,61],[22,20],"k")
+plt.plot(Xrange[:,1],Xd,"k")
+plt.axis('equal')
+plt.xlabel('X1')
+plt.ylabel('X2')
+plt.grid()
+plt.show()
+'''
 
 
 exit()
