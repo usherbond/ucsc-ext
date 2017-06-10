42fc1c831768c1fb99b3c4103653552f778f10d2
roquealex <roquealex20@hotmail.com>
2017-06-10 12:50:15 -0700

    kmeans

--- c2612p019/Assignments/Assignment6/homework6.py
+++ c2612p019/Assignments/Assignment6/homework6.py
@@ -45,8 +45,8 @@ def linear_classifier(x_vectors, W) :
 
 np.random.seed(1)
 
-excel_file = 'short.xlsx'
-#excel_file = 'Car_Data.xlsx'
+#excel_file = 'short.xlsx'
+excel_file = 'Car_Data.xlsx'
 #excel_file = 'Assignment_4_Data_and_Template.xlsx'
 
 data_frame = pd.read_excel(excel_file,sheetname='Sheet1',skiprows=1)
@@ -129,6 +129,7 @@ print  np.append(T_index,classified,1)
 
 conf_len = len(ord_dictionary['Recommendation'][1])
 
+'''
 conf_matrix = np.array(
 [[25,28,73,38,68, 0],
  [ 0, 0,57,74, 0,68],
@@ -146,7 +147,6 @@ for i, true_cl in enumerate(T_index) :
 	conf_matrix[tcl,cas] += 1
 	#row_idx = (tcl + 1)/2
 print "Confusion matrix"
-'''
 print conf_matrix
 
 conf_total = np.sum(conf_matrix)
@@ -168,7 +168,85 @@ for i in range(len(accuracy)) :
 	accuracy[i] = round((TP+TN)/float(conf_total),2)
 print "Accuracy",accuracy
 
-exit()
+# K means classification:
+
+# initializations:
+K = 2
+
+X_max = np.amax(X,axis=0)
+X_min = np.amin(X,axis=0)
+X_diff = (X_max-X_min).astype(float)
+
+print X
+print X_max,X_min
+print X_diff
+print len(X_diff)
+
+# Be careful with the type not being a matrix
+mu = (np.random.rand(K,len(X_diff)) * X_diff) + X_min
+print np.round(mu,1)
+
+C = np.zeros((X.shape[0],1),int)
+#print C.shape
+#print  np.append(T_index,C,1)
+error = 10000.0 ; tolerance = 0 ; iterations = 0
+
+while ((error>tolerance) and (iterations < 10000)) :
+	for i in range(X.shape[0]) :
+		#print X[i]
+		mu_minus_x = mu - X[i]
+		#print np.round(mu_minus_x,1)
+		# this is fast but wasteful:
+		mult = np.dot(mu_minus_x,mu_minus_x.transpose())
+		#print mult
+		diag = np.diagonal(mult)
+		#print diag
+		C[i,0] = np.argmin(diag)
+		#print C[i,0]
+
+	newmu = np.zeros((K,len(X_diff)),float)
+	for k in range(K) :
+		k_index = np.where(C==k)[0]
+		#print k_index
+		#print X[k_index]
+		if  (len(k_index)>0) :
+			newmu[k] = np.mean(X[k_index],axis=0).astype(float) #Avoiding potential issues with int
+			#print newmu[k]
+		else :
+			# controversial but use the old one:
+			newmu[k] = mu[k]
+
+	#print newmu
+	#print mu
+	mu_err = mu - newmu
+	#print mu_err
+	mu_err_abs = np.abs(mu_err)
+	#print mu_err_abs
+	error =  np.amax(mu_err_abs)
+	print error
+
+	mu = newmu
+	iterations += 1
+	#print mu
+
+print  np.append(T_index,C,1)
+print  iterations, error
+print C
+print np.bincount(C.flatten())
+
+for k in range(K) :
+	k_index = np.where(C==k)[0]
+	print "Composition of mean k =",k
+	#print T_index[k_index]
+	bins = np.bincount(T_index[k_index].flatten())
+	print bins
+
+#X_mean = np.mean(X,axis=0).astype(float) #Avoiding potential issues with int
+
+#index_n = np.where(T==clabel_n)[0]
+
+print mu
+
 	
 
 '''
@@ -330,473 +408,9 @@ mark = ['_','|','x','o','|','x']
 alpha_cloud = 0.1
 for i,cl in enumerate(Cset) :
 	plt.scatter(cl[:,0],cl[:,1],color=col[i],marker=mark[i],alpha=alpha_cloud)
-#plt.scatter(P_red[index_n][:,0],P_red[index_n][:,1],color="g",marker="o",alpha=alpha_cloud)
-#plt.scatter(P_red[index_p][:,0],P_red[index_p][:,1],color="b",marker="o",alpha=alpha_cloud)
 plt.axis('equal')
 plt.grid()
 
 plt.show()
 exit()
-
-# Classifier related:
-P_red_mean_n = np.mean(P_red_n,axis=0).astype(float)
-P_red_mean_p = np.mean(P_red_p,axis=0).astype(float)
-print "Mean P vector negative"
-print P_red_mean_n
-print "Mean P vector positive"
-print P_red_mean_p
-
-# Covar:
-P_red_cov_n = np.cov(P_red_n, rowvar=False, ddof=1)
-P_red_cov_p = np.cov(P_red_p, rowvar=False, ddof=1)
-print "Covar P vector negative"
-print P_red_cov_n
-print "Covar P vector positive"
-print P_red_cov_p
-
-# Histogram:
-#calculating number of bins:
-#total_min = min(total_p, total_n)
-# Assuming half and half
-#print P_red.shape[0]
-B = int(np.ceil(np.log(P_red.shape[0]/2)/np.log(2))) +1
-print "Number of bins:",B
-
-# first number is number of rows and that will be used for first eigenvector
-hist_n = np.zeros((B,B),int)
-hist_p = np.zeros((B,B),int)
-
-# For translation from previos hw Heigth = 0 and HandSpan = 1
-print "Max:",P_red_max
-print "Min:",P_red_min
-
-#print P_red_n
-for row in P_red_n :
-	#print "Negative training set",row
-	hist_row = bin_index(row[0],B,P_red_min[0],P_red_max[0])
-	hist_col = bin_index(row[1],B,P_red_min[1],P_red_max[1])
-	hist_n[hist_row,hist_col]+=1
-
-#print P_red_p
-for row in P_red_p :
-	#print "Positive training set",row
-	hist_row = bin_index(row[0],B,P_red_min[0],P_red_max[0])
-	hist_col = bin_index(row[1],B,P_red_min[1],P_red_max[1])
-	hist_p[hist_row,hist_col]+=1
-
-print "Negative hist"
-print hist_n
-print "Positive hist"
-print hist_p
-
-'''
-# Scatter for homework:
-plt.figure(1)
-alpha_cloud = 0.1
-plt.scatter(P_red[index_n][:,0],P_red[index_n][:,1],color="r",marker="o",alpha=alpha_cloud)
-plt.scatter(P_red[index_p][:,0],P_red[index_p][:,1],color="b",marker="o",alpha=alpha_cloud)
-plt.axis('equal')
-plt.grid()
-#plt.show()
-plot_histogram(hist_n, hist_p,P_red_max[0],P_red_min[0],P_red_max[1],P_red_min[1])
-'''
-
-# Trying 2 samples of each
-print np.append(X, T,1)
-print index_n
-print index_p
-
-sample_idx_n = np.asscalar(np.random.choice(index_n,1))
-print "Negative sample:",sample_idx_n
-
-#print X_mean
-x_n = X[sample_idx_n]
-z_n = x_n - X_mean
-p_n = np.dot(z_n,V_t)
-p_red_n =p_n[0:2]
-#r_n = np.dot(p_n,V_t.T)
-#print V_t
-#print V_t.T[0:2]
-r_n = np.dot(p_red_n,V_t.T[0:2])
-xrec_n = (r_n + X_mean)
-xrec_pro_n = np.round(np.clip(xrec_n,0,255)).astype(int)
-print "Negative feature vec",x_n
-print "Cent Neg feature vec",z_n
-print "P Neg feature vec",p_n
-print "P red Neg feature vec",p_red_n
-print "Rec Z Neg feature vec",r_n
-print "Rec X Neg feature vec",xrec_n
-print "Rec X Neg feature vec",xrec_pro_n
-
-sample_idx_p = np.asscalar(np.random.choice(index_p,1))
-print "Positive sample:",sample_idx_p
-
-x_p = X[sample_idx_p]
-z_p = x_p - X_mean
-p_p = np.dot(z_p,V_t)
-p_red_p =p_p[0:2]
-r_p = np.dot(p_red_p,V_t.T[0:2])
-xrec_p = (r_p + X_mean)
-xrec_pro_p = np.round(np.clip(xrec_p,0,255)).astype(int)
-#xrec_pro_p = xrec_p
-print "Positive feature vec",x_p
-print "Cent Pos feature vec",z_p
-print "P Pos feature vec",p_p
-print "P red Pos feature vec",p_red_p
-print "Rec Z Pos feature vec",r_p
-print "Rec X Pos feature vec",xrec_p
-print "Rec X Pos feature vec",xrec_pro_p
-
-#print P_red
-
-
-'''
-# Plot numbers original and recovered
-if (X.shape[1] == 28*28) :
-	img_shape = (28,28)
-else :
-	img_shape = (1, X.shape[1])
-plt.subplot(2,2,1)
-plt.imshow(x_n.reshape(img_shape),interpolation='None', cmap=plt.get_cmap('gray'))
-plt.subplot(2,2,2)
-plt.imshow(xrec_pro_n.reshape(img_shape),interpolation='None', cmap=plt.get_cmap('gray'))
-plt.subplot(2,2,3)
-plt.imshow(x_p.reshape(img_shape),interpolation='None', cmap=plt.get_cmap('gray'))
-plt.subplot(2,2,4)
-plt.imshow(xrec_pro_p.reshape(img_shape),interpolation='None', cmap=plt.get_cmap('gray'))
-plt.show()
-'''
-
-#queries
-
-'''
-print P_red_mean_n
-print P_red_mean_p
-print P_red_cov_n
-print P_red_cov_p
-print p_red_n
-print p_red_p
-
-p_red_n_pdf_n = norm_pdf(p_red_n,P_red_mean_n,P_red_cov_n)
-p_red_n_pdf_p = norm_pdf(p_red_n,P_red_mean_p,P_red_cov_p)
-print p_red_n_pdf_n
-print p_red_n_pdf_p
-
-p_red_n_pcnt_n = total_n * p_red_n_pdf_n
-p_red_n_pcnt_p = total_p * p_red_n_pdf_p
-
-print p_red_n_pcnt_n
-print p_red_n_pcnt_p
-
-print "Total samples of N (",clabel_n,"):",total_n
-print "Total samples of P (",clabel_p,"):",total_p
-'''
-
-res_bayes_x_n = bayes_clasifier(p_red_n, (P_red_mean_n,P_red_mean_p), (P_red_cov_n,P_red_cov_p), (total_n,total_p), (clabel_n,clabel_p))
-print 'Result for bayes xn',res_bayes_x_n,'supposed to be',T[sample_idx_n]
-
-res_bayes_x_p = bayes_clasifier(p_red_p, (P_red_mean_n,P_red_mean_p), (P_red_cov_n,P_red_cov_p), (total_n,total_p), (clabel_n,clabel_p))
-print 'Result for bayes xp',res_bayes_x_p,'supposed to be',T[sample_idx_p]
-
-
-res_hist_x_n = histogram_clasifier(p_red_n, (hist_n,hist_p),(clabel_n,clabel_p),P_red_min, P_red_max)
-print 'Result for histogram xn',res_hist_x_n,'supposed to be',T[sample_idx_n]
-res_hist_x_p = histogram_clasifier(p_red_p, (hist_n,hist_p),(clabel_n,clabel_p),P_red_min, P_red_max)
-print 'Result for histogram xp',res_hist_x_p,'supposed to be',T[sample_idx_p]
-
-# Measuring training accuracy:
-print "Measuring Accuracy for histogram"
-TP_hist = 0
-TN_hist = 0
-FP_hist = 0
-FN_hist = 0
-for i,q in enumerate(P_red) :
-	class_output = histogram_clasifier(q, (hist_n,hist_p),(clabel_n,clabel_p),P_red_min, P_red_max)
-	ground_truth = np.asscalar(T[i])
-	if (ground_truth==clabel_n) :
-		if (class_output[0]==clabel_n) :
-			status = "True Negative"
-			TN_hist +=1
-		else :
-			status = "False Positive"
-			FP_hist +=1
-	else :
-		if (class_output[0]==clabel_p) :
-			status = "True Positive"
-			TP_hist +=1
-		else :
-			status = "False Negative"
-			FN_hist +=1
-	print class_output, ground_truth, status
-del class_output
-#print class_output
-#s/\(\w\+\)/print '\1',\1
-print 'TP_hist',TP_hist
-print 'TN_hist',TN_hist
-print 'FP_hist',FP_hist
-print 'FN_hist',FN_hist
-
-total_hist = TP_hist + TN_hist + FP_hist + FN_hist
-accuracy_hist = float(TP_hist + TN_hist)/total_hist
-
-#Verify
-if (X.shape[0] != total_hist) :
-	print("The accuracy test for histogram is not successful")
-	exit(0)
-
-print "Accuracy for histogram",accuracy_hist
-
-# Measuring training accuracy bayes:
-print "Measuring Accuracy for bayes"
-TP_bayes = 0
-TN_bayes = 0
-FP_bayes = 0
-FN_bayes = 0
-for i,q in enumerate(P_red) :
-	#class_output = histogram_clasifier(q, (hist_n,hist_p),(clabel_n,clabel_p),P_red_min, P_red_max)
-
-	class_output = bayes_clasifier(q, (P_red_mean_n,P_red_mean_p), (P_red_cov_n,P_red_cov_p), (total_n,total_p), (clabel_n,clabel_p))
-
-	ground_truth = np.asscalar(T[i])
-	if (ground_truth==clabel_n) :
-		if (class_output[0]==clabel_n) :
-			status = "True Negative"
-			TN_bayes +=1
-		else :
-			status = "False Positive"
-			FP_bayes +=1
-	else :
-		if (class_output[0]==clabel_p) :
-			status = "True Positive"
-			TP_bayes +=1
-		else :
-			status = "False Negative"
-			FN_bayes +=1
-	print class_output, ground_truth, status
-del class_output
-#print class_output
-#s/\(\w\+\)/print '\1',\1
-print 'TP_bayes',TP_bayes
-print 'TN_bayes',TN_bayes
-print 'FP_bayes',FP_bayes
-print 'FN_bayes',FN_bayes
-
-total_bayes = TP_bayes + TN_bayes + FP_bayes + FN_bayes
-accuracy_bayes = float(TP_bayes + TN_bayes)/total_bayes
-
-#Verify
-if (X.shape[0] != total_bayes) :
-	print("The accuracy test for bayes is not successful")
-	exit(0)
-
-print "Accuracy for bayes",accuracy_bayes
-
-
-
-
-#print T
-
-
-
-#end of computation
-
-
-'''
-'''
-# Writing Excel file:
-row = 0
-
-workbook = xlsxwriter.Workbook('results.xlsx')
-worksheet = workbook.add_worksheet()
-
-worksheet.set_column('A:A', 35)
-
-worksheet.write(row, 0, 'Roque Alejandro Arcudia Hernandez')
-row +=1
-worksheet.write(row, 0, 'mu (mean vector)')
-for i in range(len(X_mean)) :
-	worksheet.write(row, 1+i, X_mean[i])
-
-row +=1
-worksheet.write(row, 0, 'v1 (First eigenvector)')
-'''
-V_t = np.array(
-[[1, 2, 3],
- [4, 5, 6],
- [7, 8, 9],
- [10, 11, 12]]
-)
-print V_t
-print V_t.shape
-'''
-for i in range(V_t.shape[0]) :
-	worksheet.write(row, 1+i, V_t[i,0])
-
-row +=1
-worksheet.write(row, 0, 'v2 (Second eigenvector)')
-for i in range(V_t.shape[0]) :
-	worksheet.write(row, 1+i, V_t[i,1])
-
-row +=2
-worksheet.write(row, 0, 'Np (class +1 number of samples)')
-worksheet.write(row, 1, total_p)
-row +=1
-worksheet.write(row, 0, 'Nn (class -1 number of samples)')
-worksheet.write(row, 1, total_n)
-
-row +=2
-worksheet.write(row, 0, 'mup (class +1 mean vector)')
-for i in range(len(P_red_mean_p)) :
-	worksheet.write(row, 1+i, P_red_mean_p[i])
-row +=1
-worksheet.write(row, 0, 'mun (class -1 mean vector)')
-for i in range(len(P_red_mean_n)) :
-	worksheet.write(row, 1+i, P_red_mean_n[i])
-
-row +=2
-worksheet.write(row, 0, 'cp (class +1 covariance matrix)')
-'''
-P_red_cov_p = np.array(
-[[1, 2],
- [3, 4],
- [5, 6]]
-)
-print P_red_cov_p
-print P_red_cov_p.shape
-'''
-for i in range(P_red_cov_p.shape[1]) :
-	for j in range(P_red_cov_p.shape[0]) :
-		worksheet.write(row+j, 1+i, P_red_cov_p[j,i])
-
-row +=2
-worksheet.write(row, 0, 'cn (class -1 covariance matrix)')
-for i in range(P_red_cov_n.shape[1]) :
-	for j in range(P_red_cov_n.shape[0]) :
-		worksheet.write(row+j, 1+i, P_red_cov_n[j,i])
-
-row +=3
-worksheet.write(row, 0, 'Histogram range, pc1 direction')
-worksheet.write(row, 1, P_red_min[0])
-worksheet.write(row, 2, P_red_max[0])
-row +=1
-worksheet.write(row, 0, 'Histogram range, pc2 direction')
-worksheet.write(row, 1, P_red_min[1])
-worksheet.write(row, 2, P_red_max[1])
-
-row +=1
-worksheet.write(row, 0, 'Size of histograms')
-worksheet.write(row, 1, '25 x 25')
-
-row +=1
-worksheet.write(row, 0, 'Hp (class +1 histogram)')
-'''
-hist_p = np.array(
-[[1, 2],
- [3, 4],
- [5, 6]]
-)
-print hist_p
-print hist_p.shape
-'''
-for i in range(hist_p.shape[1]) :
-	for j in range(hist_p.shape[0]) :
-		worksheet.write(row+j, 1+i, hist_p[j,i])
-
-
-row +=26
-worksheet.write(row, 0, 'Hn (class -1 histogram)')
-for i in range(hist_n.shape[1]) :
-	for j in range(hist_n.shape[0]) :
-		worksheet.write(row+j, 1+i, hist_n[j,i])
-
-row +=28
-worksheet.write(row, 0, 'xp (class +1 feature vector)')
-for i in range(len(x_p)) :
-	worksheet.write(row, 1+i, x_p[i])
-row +=1
-worksheet.write(row, 0, 'zp (centered feature vector)')
-for i in range(len(z_p)) :
-	worksheet.write(row, 1+i, z_p[i])
-row +=1
-worksheet.write(row, 0, 'pp (2 dim representation)')
-for i in range(len(p_red_p)) :
-	worksheet.write(row, 1+i, p_red_p[i])
-row +=1
-worksheet.write(row, 0, 'rp (reconstructed zp)')
-for i in range(len(r_p)) :
-	worksheet.write(row, 1+i, r_p[i])
-row +=1
-worksheet.write(row, 0, 'xrecp (reconstructed xp)')
-for i in range(len(xrec_pro_p)) :
-	worksheet.write(row, 1+i, xrec_pro_p[i])
-
-row +=2
-worksheet.write(row, 0, 'xn (class -1 feature vector)')
-for i in range(len(x_n)) :
-	worksheet.write(row, 1+i, x_n[i])
-row +=1
-worksheet.write(row, 0, 'zn (centered feature vector)')
-for i in range(len(z_n)) :
-	worksheet.write(row, 1+i, z_n[i])
-row +=1
-worksheet.write(row, 0, 'pn (2 dim representation)')
-for i in range(len(p_red_n)) :
-	worksheet.write(row, 1+i, p_red_n[i])
-row +=1
-worksheet.write(row, 0, 'rn (reconstructed zn)')
-for i in range(len(r_n)) :
-	worksheet.write(row, 1+i, r_n[i])
-row +=1
-worksheet.write(row, 0, 'xrecn (reconstructed xn)')
-for i in range(len(xrec_pro_n)) :
-	worksheet.write(row, 1+i, xrec_pro_n[i])
-
-row +=4
-worksheet.write(row, 0, 'Actual digit represented by xp')
-worksheet.write(row, 1, T[sample_idx_p])
-row +=1
-worksheet.write(row, 0, 'Result of classifying xp using histograms')
-worksheet.write(row, 1, res_hist_x_p[0])
-worksheet.write(row, 2, res_hist_x_p[1])
-row +=1
-worksheet.write(row, 0, 'Result of classifying xp using Bayesian')
-worksheet.write(row, 1, res_bayes_x_p[0])
-worksheet.write(row, 2, res_bayes_x_p[1])
-
-row +=2
-worksheet.write(row, 0, 'Actual digit represented by xn')
-worksheet.write(row, 1, T[sample_idx_n])
-row +=1
-worksheet.write(row, 0, 'Result of classifying xn using histograms')
-worksheet.write(row, 1, res_hist_x_n[0])
-worksheet.write(row, 2, res_hist_x_n[1])
-row +=1
-worksheet.write(row, 0, 'Result of classifying xn using Bayesian')
-worksheet.write(row, 1, res_bayes_x_n[0])
-worksheet.write(row, 2, res_bayes_x_n[1])
-
-row +=3
-worksheet.write(row, 0, 'Training accuracy attained using histograms')
-worksheet.write(row, 1, accuracy_hist)
-row +=1
-worksheet.write(row, 0, 'Training accuracy attained using Bayesian')
-worksheet.write(row, 1, accuracy_bayes)
-
-'''
-worksheet.write(row, 0, 'Hn (class -1 histogram)')
-%s/\(.*\)/worksheet.write(row, 0, '\1')
-worksheet.write(row, 0, '')
-
-'''
-
-
-workbook.close()
-
-'''
-Questions for prof:
-	- In the reconstructed image xrec, should we clip the image if one of the pixels is less than 0 and one is greater than 255. Should we make them integer
-	- Hhich probability do we want? the probability of positive or the probability that won the query?
-	-What if we get a singular matrix as covariance
-
-'''
 
